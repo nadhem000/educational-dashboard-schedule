3aeba7f78f1e6b62aba9573181f4b46d7f6b3fcd
MejriZiad <mejri.ziad@gmail.com>
2026-08-24 10:47:28 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // EDSchedule Service Worker – development-friendly caching
 // Version bumped to v4 to clear old cache that might contain Supabase responses
-const CACHE_NAME = 'edschedule-cache-v39b';
+const CACHE_NAME = 'edschedule-cache-v39c';
 
 // Core assets to pre-cache on install
 const CORE_ASSETS = [
